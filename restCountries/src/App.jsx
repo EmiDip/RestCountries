b2eb0368cf4e5a4c547733a095fedc1ce2214fed
EmiDip <di.palma.emilie@outlook.fr>
2023-11-23 15:26:36 +0100

    Connection Api

--- restCountries/src/App.jsx
+++ restCountries/src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchCountriesData } from "./api/api";
+import { Card } from "./components/Card";
 import "./App.css";
 
 function App() {
-  return <></>;
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const countriesData = await fetchCountriesData();
+        console.log("Countries Data:", countriesData);
+      } catch (error) {
+        // Gérer les erreurs si nécessaire
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+}
 export default App;
---
+++ restCountries/src/components/Card.jsx
@@ -0,0 +1,10 @@
+export const Card = () => {
+  return (
+    <div>
+      <div>image</div>
+      <div>pays</div>
+      <div>capital</div>
+      <div>continent</div>
+    </div>
+  );
+};
